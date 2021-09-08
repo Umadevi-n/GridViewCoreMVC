badc53fbd1821a65a75e331b6db23168e75ff2ef
Quadrant <Quadrant@DESKTOP-FEKK5US>
2021-09-08 15:54:24 +0530

    Add project files.

---
+++ GridViewCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using GridViewCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GridViewCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBcontext Context { get; }
+        public HomeController(DBcontext _context)
+        {
+            this.Context = _context;
+        }
+
+
+        public IActionResult Index(int p = 1)
+        {
+            PagedResult<Employee> customers = this.Context.Employee.GetPaged(p, 10);
+            return View(customers);
+        }
+
+    }
+}
---
+++ GridViewCoreMVC/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GridViewCoreMVC.Models
+{
+    public class Employee
+    {
+        
+        [Key] // indicate EF that this is key of table, as it doesn't have usualy "Id" column as PK
+        public int EmpID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
+
